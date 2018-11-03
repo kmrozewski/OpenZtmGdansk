fe66b28cfe01b4ea1487b9d284bfe0b878cd6394
Konrad Mrozewski <konradmrozewski@carfax.com>
2018-11-03 23:40:25 +0100

    fixed problems with stops file encoding for 'Ł', singleton for stopMapBean

--- ztm-services/src/main/java/com/opengdansk/search/conguration/StopMapBean.java
+++ ztm-services/src/main/java/com/opengdansk/search/conguration/StopMapBean.java
@@ -15,16 +15,28 @@ import static com.opengdansk.search.reader.JsonReader.parseFixtureToCollection;
 public class StopMapBean {
 
     private static final String STOPS_AGGREGATED_BY_NAME_AND_CODE_PATH = "stops_nested.json";
-    private final Map<String, List<StopCodeAgg>> stopWithNamesMap = parseFixtureToCollection(
-            this.getClass(),
-            StopNameAgg.class,
-            List.class,
-            STOPS_AGGREGATED_BY_NAME_AND_CODE_PATH)
-            .stream()
-            .collect(Collectors.toMap(StopNameAgg::getName, StopNameAgg::getCodes));
+    private Map<String, List<StopCodeAgg>> stopWithNamesMap;
 
     @Bean
     public Map<String, List<StopCodeAgg>> getStopsMap() {
+        if (stopWithNamesMap == null) {
+            stopWithNamesMap = mapListToStopsMap();
+        }
+
         return stopWithNamesMap;
+    }
+
+    private Map<String, List<StopCodeAgg>> mapListToStopsMap() {
+        return readListWithStops()
+                .stream()
+                .collect(Collectors.toMap(StopNameAgg::getName, StopNameAgg::getCodes));
+    }
+
+    private List<StopNameAgg> readListWithStops() {
+        return parseFixtureToCollection(
+                this.getClass(),
+                StopNameAgg.class,
+                List.class,
+                STOPS_AGGREGATED_BY_NAME_AND_CODE_PATH);
     }
 }
