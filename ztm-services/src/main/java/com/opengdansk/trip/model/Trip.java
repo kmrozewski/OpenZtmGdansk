c7f4de5a515ac757e51ec8605934f89516da2be1
Konrad Mrozewski <konradmrozewski@carfax.com>
2019-10-25 13:13:43 +0200

    clean up trip headsign string

--- ztm-services/src/main/java/com/opengdansk/trip/model/Trip.java
+++ ztm-services/src/main/java/com/opengdansk/trip/model/Trip.java
@@ -1,6 +1,7 @@
 package com.opengdansk.trip.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +14,23 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Trip {
 
+    private static final String REGEX_NUMBER_IN_PARENTHESES = "\\([0-9]+\\)";
+    private static final String REGEX_SPECIAL_CHARS = "[\\@\\+]+";
+    private static final String REGEX_WORD = "new\\.";
+
     private String id;
     private Integer routeId;
+
     private String tripHeadsign;
     private String tripShortName;
     private Integer directionId;
+
+
+    @JsonProperty("tripHeadsign")
+    public void setTripHeadsign(String tripHeadsign) {
+        this.tripHeadsign = tripHeadsign
+                .replaceAll(REGEX_NUMBER_IN_PARENTHESES, "")
+                .replaceAll(REGEX_SPECIAL_CHARS, "")
+                .replaceAll(REGEX_WORD, "");
+    }
 }
