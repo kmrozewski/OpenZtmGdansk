89847df2005326568a1a9fc0a5c0464bc49dc732
Konrad Mrozewski <konradmrozewski@carfax.com>
2020-02-14 08:53:38 +0100

    spinner styling

--- frontend/src/spinner/withSpinner.jsx
+++ frontend/src/spinner/withSpinner.jsx
@@ -2,10 +2,23 @@
 //https://www.robinwieruch.de/conditional-rendering-react#conditional-rendering-with-hoc
 import React from "react";
 
+const style = {
+    width: "6rem",
+    height: "6rem",
+    marginTop: "1rem",
+    marginBottom: "1rem",
+}
+
 export default class Spinner extends React.Component {
     render() {
         return (
-            <p>Loading...</p>
+            <div className="d-flex justify-content-center">
+                <div className="spinner-border text-primary"
+                     style={style}
+                     role="status">
+                    <span className="sr-only">Loading...</span>
+                </div>
+            </div>
         )
     }
 }
